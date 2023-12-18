656d291dddf337d879f06066758f25567096b9d6
Emre Tufan <emretufan42@gmail.com>
2023-12-18 17:51:57 +0300

    refactor: add forward ref and props on button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,16 @@
+import { forwardRef } from "react";
 import styles from "./Button.module.css";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
 }
 
-const Button = ({ children }: ButtonProps) => {
-  return <button className={styles.button}>{children}</button>;
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }, ref) => {
+  return (
+    <button ref={ref} className={styles.button} {...props}>
+      {children}
+    </button>
+  );
+});
 
 export default Button;
